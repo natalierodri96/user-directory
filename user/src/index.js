45878e7b90f671e082a4a13c09ee6c292e66ccdd
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-03 22:44:06 -0600

    changes

--- user/src/index.js
+++ user/src/index.js
@@ -1,4 +1,4 @@
 import React from "react"
-import ReactDOM, { render } from "react-dom"
+import ReactDOM from "react-dom"
 
-ReactDOM.render(<h1></h1>, document.getElementById("root"))
+ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"))

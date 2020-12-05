88a2b8eec54430e721a7673f93bcd79ff58d6e06
Natalie Rodriguez <natalierodri96@gmail.com>
2020-12-05 12:19:50 -0600

    new

--- userdirectory/src/components/table.js
+++ userdirectory/src/components/table.js
@@ -1,6 +1,6 @@
 import React from "react";
 import API from "../utils/api.js"
-import Search from "../components/searchBar"
+import Search from "../components/navbar"
 import "../styles/table.css"
 import DateFormat from 'dateformat';
 
